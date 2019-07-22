8bcad651fd34dbcaf1dd7cb718c72eb3379407f9
vidhisharma3193 <vidhisharma3193@gmail.com>
2019-07-22 14:16:29 -0500

    rails-forms

---
+++ 18-Rails-Forms/db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Dragon.create(name: "Tomar", age: 2055)
+Dragon.create(name: "Dojo", age: 1905)
+Dragon.create(name: "Naush", age: 2081)
+Dragon.create(name: "Tj", age: 3000)
+Dragon.create(name: "Bojo", age: 5060)
