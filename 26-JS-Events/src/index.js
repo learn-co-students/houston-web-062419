679f0c25d867e496a77b4d34e5e42d639b736a11
vidhisharma3193 <vidhisharma3193@gmail.com>
2019-08-06 11:09:11 -0500

    Lecture-done

---
+++ 26-JS-Events/src/index.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const btn = document.querySelector("#first-btn")
+    console.log(btn)
+
+    btn.addEventListener("click", function() {
+        console.log("Hi from us to you")
+    })
+
+    const form = document.querySelector("#comment-form")
+    
+    form.addEventListener("submit", function(event){
+        event.preventDefault()
+        // console.log(event.target[0].value)
+        let comment = event.target[0].value
+        const div = document.querySelector("#comments-container")
+
+        const p = document.createElement("p")
+        p.innerText = comment
+
+        div.append(p)
+        // event.target[0].value = ""
+        event.target.reset()
+        // debugger
+    })
+
+    const divTag = document.querySelector("#helicopter-parent")
+
+    divTag.addEventListener("click", function(event){
+        // debugger
+
+        if(event.target.dataset.name === "alert"){
+            window.alert("It is an alert box!!!")
+        }
+    })
+})
+
+
+
+
+
+
+
+
+
