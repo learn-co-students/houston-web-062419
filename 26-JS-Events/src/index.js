e53f3f62352a9d307a0a5e74be41ae809e13264e
vidhisharma3193 <vidhisharma3193@gmail.com>
2019-08-06 15:26:17 -0500

    task solution

--- 26-JS-Events/src/index.js
+++ 26-JS-Events/src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const btn = document.querySelector("#first-btn")
-    console.log(btn)
+    // console.log(btn)
 
     btn.addEventListener("click", function() {
         console.log("Hi from us to you")
@@ -28,15 +28,31 @@ document.addEventListener("DOMContentLoaded", function() {
     divTag.addEventListener("click", function(event){
         // debugger
 
-        if(event.target.dataset.name === "alert"){
-            window.alert("It is an alert box!!!")
+        // Refactored code
+        switch(event.target.dataset.name){
+            case "alert":
+                window.alert("It is an alert box!!!")
+                break;
+            case "log":
+                console.log("It is a console log!!!")
+                break;
+            case "error":
+                console.error("It is a console error!!!")
+                break;
+            default:
+                console.log("not clicked on the buttons")
         }
-        if(event.target.dataset.name === "log"){
-            console.log("It is a console log!!!")
-        }
-        if(event.target.dataset.name === "error"){
-            console.error("It is a console error!!!")
-        }
+
+
+        // if(event.target.dataset.name === "alert"){
+        //     window.alert("It is an alert box!!!")
+        // }
+        // if(event.target.dataset.name === "log"){
+        //     console.log("It is a console log!!!")
+        // }
+        // if(event.target.dataset.name === "error"){
+        //     console.error("It is a console error!!!")
+        // }
     })
 })
 
