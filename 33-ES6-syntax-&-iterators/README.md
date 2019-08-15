a5955efcc7a3b7ab3fce6f109327c46b7154afe7
vidhisharma3193 <vidhisharma3193@gmail.com>
2019-08-15 17:12:51 -0500

    ES6 and iterators

--- 33-ES6-syntax-&-iterators/README.md
+++ 33-ES6-syntax-&-iterators/README.md
@@ -1,1 +1,96 @@
-# Es6 Syntax & Iterators
+# ES6 syntax & Iterators
+
+![](https://media.giphy.com/media/13twUEuUnCrEju/giphy.gif)
+
+---
+
+## The Following are Used heavily in Mod 4 for React (and are important to know if you're writing modern JavaScript):
+
+#### destructuring
+
+```js
+const spaceship = {
+    pilot: 'elon musk',
+    guidance: 'marc zucc'
+}
+/* from the object spaceship, please pull out the VALUES stored at pilot and guidance */
+const { pilot, guidance } = spaceship
+
+console.log(pilot) // 'elon musk'
+console.log(guidance) // 'marc zucc'
+
+```
+
+---
+
+#### key value assignment shortcut
+
+```javascript
+const pizza = 'meat-lover'
+const restaurant = 'New York Pizza™️'
+
+const pizzaObj = {
+  pizza: pizza,
+  restaurant: restaurant
+}
+```
+
+---
+
+#### ES6 Spread Operator
+
+```js
+// Array
+let a = [1,2,3]
+let b = [4,5,6]
+
+let numbers = [...a, ...b] //[1,2,3,4,5,6]
+
+// Objects
+
+let obj1 = {name: "student", location:"NYC"}
+let obj2 = {school: "flatironschool"}
+
+let student = {...obj1, ...obj2} //{name: "student", location: "NYC", school: "flatironschool"}
+
+// How to change single key inside an object?
+student.location = "Houston"
+
+// OR
+student = {...student, location: "Houston"} // {name: "student", location: "Houston", school: "flatironschool"}
+
+```
+
+```js
+function sum(x, y, z) {
+  return x + y + z;
+}
+
+const numbers = [1, 2, 3];
+
+console.log(sum(...numbers));
+// expected output: 6
+```
+---
+
+#### all the forms of arrow functions
+
+```javascript
+
+const implicitReturn = () => 'hi'
+const explicitReturn = () => {
+  return 'hi'
+}
+
+```
+
+---
+
+#### ES6 Iterators
+* .forEach
+* .map
+* .find
+* .filter
+* .reduce
+* for...in
+* for...of
--- 33-ES6-syntax-&-iterators/index.js
+++ 33-ES6-syntax-&-iterators/index.js
@@ -1,36 +1,154 @@
+// Destructuring
+const spaceship = {
+    pilot: 'elon musk',
+    guidance: 'marc zucc'
+}
+/* from the object spaceship, please pull out the VALUES stored at pilot and guidance */
+const { pilot, guidance } = spaceship
+
+console.log(pilot) // 'elon musk'
+console.log(guidance) // 'marc zucc'
+
+// Key value assignment shortcut
+const pizza = 'meat-lover'
+const restaurant = 'New York Pizza™️'
+
+const pizzaObj = {
+  pizza: pizza,
+  restaurant: restaurant
+}
+
+// name of key is the same as the name of the variable we wish to assign to that key
+const pizzaObj2 = { pizza, restaurant }
+
+// ES6 spread operators
+
+// Array
+let a = [1,2,3]
+let b = [4,5,6]
+
+let numbers = [...a, ...b] //[1,2,3,4,5,6]
+
+// Objects
+
+let obj1 = {name: "student", location:"NYC"}
+let obj2 = {school: "flatironschool"}
+
+let student = {...obj1, ...obj2} //{name: "student", location: "NYC", school: "flatironschool"}
+
+// How to change single key inside an object?
+student.location = "Houston"
+
+// OR
+student = {...student, location: "Houston"} // {name: "student", location: "Houston", school: "flatironschool"}
+
+// Arrow functions
+const implicitReturn = () => 'hi'
+
+const explicitReturn = () => {
+  return 'hi'
+}
+
+// ES6 Iterators
+console.log("Iterators")
+
+const arr = [1,2,3,4,5]
+
+// Iterate through an array
+
+// .forEach => iterate through an array but not returnin anything
+
+arr.forEach((number) => console.log(number))
+
+// .map => iterate through an array and returns an array with the modification provided in the callback funcation
+
+arr.map((number) => number * 2) //[2,4,6,8,10]
+
+// .find => returns first element from an array which passes the condition
+
+arr.find((number) => number % 2 === 0) // 2
+
+// .filter => returns an array which include all the elements which passes the condition
+
+arr.filter((number) => number % 2 === 0) //[2,4] 
+
+// .reduce => takes a callback function as an argument which require two arguments: 1. Accumulator & 2. array elment -> current element
+
+arr.reduce((acc,number) => acc + number) // 15 => for addition acc starts with 0
+arr.reduce((acc,number) => { return acc + number}, 10) // 25 => start acc with 10
 
 
+// Iterate through an object
+const obj = {
+    a:10,
+    b:20,
+    c:30,
+    d:40
+}
 
+for(const key in obj){
+    console.log(obj[key])
+}
+// 10
+// 20
+// 30
+// 40
 
+// Iterate through an array using for...in
+for(const index in arr){
+    console.log(arr[index])
+}
+// 1
+// 2
+// 3
+// 4
+// 5
 
+// Iterate through an string using for...in
+const string = "String is the coolest"
 
+for(const index in string){
+    console.log(string[index]) // prints each character of string on new line in the console
+}
 
+// Iterate through an array using for...of
+for(const number of arr){
+    console.log(number)
+}
+// 1
+// 2
+// 3
+// 4
+// 5
+
+// Iterate through an string using for...of
+for(const char of string){
+    console.log(char)
+}
+
+// Iteration through an object using for...of is not possible
 
 //Variable scope
 let text = "062419"
 
 function x(){
-    // console.log("I am in x function",text)
-    let a = "Am I available?"
-    console.log("A from x function", a)
+    console.log("I am in x function",text)
+    let i = "Am I available?"
+    console.log("i from x function", i)
 
     function test(){
-    console.log("A from test function", a)
+    console.log("i from test function", i)
     }
 
     test()
 }
 
 function y(){
-    // console.log("I am in y function",text)
-    x()
-    console.log("A displayed y function", a)
-
+    console.log("I am in y function",text)
+    console.log("i displayed y function", i) //generate an error
 }
-
-
 
 x()
 y()
 
-console.log("A displayed outside of function", a)
+console.log("i displayed outside of function", i) //generate an error
