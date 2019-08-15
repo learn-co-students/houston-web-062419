01c5a440e4e79add34d64f31f12268ffe46bb59c
vidhisharma3193 <vidhisharma3193@gmail.com>
2019-08-15 15:41:37 -0500

    functional JS

---
+++ 34-functional-JS/README.md
@@ -0,0 +1,34 @@
+# Functional JS
+
+### Functions are Objects
+* They are objects
+* Can do anything you can do with other objects
+* Store in variables
+* Pass as arguments to other functions
+* Return from functions
+* Arrow Function
+
+### Highorder functions:
+* Function that accept other functions as arguments and/or return a function
+
+### Callback functions:
+* Functions passed as an argument to another function or A callback is a function that is to be executed after another function has finished executing
+
+### Declarative versus imperative
+
+### Pure functions
+* The function always returns the same result if the same arguments are passed in. It does not depend on any state, or data, change during a program’s execution. It must only depend on its input arguments
+* Has no side-effects, meaning it doesn't change anything in the program
+
+### composition
+* Combine two or more functions to perform some computation
+
+### Recursion
+* Function calling itself 
+
+## Tasks:
+1. For each number in the array console log "Number is:" followed by the number
+2. Return a new array with cube of numbers from current array
+3. Return all the even numbers from the array
+4. Return only first odd number from the array
+5. Multiply first two numbers from the array and then multiply the result with next number
---
+++ 34-functional-JS/index.js
@@ -0,0 +1,95 @@
+//Find reading in readme
+
+// Higher Order Function
+function highOrder(){
+    return function(){
+        console.log("I am a highorder function")
+    }
+}
+
+// Higher Order Function
+function higherOrder(callback) {
+    console.log('From Inside of Higher Order');
+    callback();
+}
+  
+// Callback
+function logHello() {
+    console.log('Hello');
+}
+
+// Passing callback into higher-order function
+higherOrder(logHello)
+
+
+// Storage in other data structure
+function funky() { console.log("Funky"); }
+function pizza() { console.log("Pizza"); }
+
+let array = [];
+
+array.push(funky);
+array.push(pizza);
+
+array[0](); // => "Funky"
+array[1](); // => "Pizza"
+
+let obj = {};
+
+obj.cool = funky;
+obj.pizza = pizza;
+
+obj.cool(); // => "Funky"
+obj.pizza(); // => "Pizza"
+
+
+// Declarative versus imperative
+
+// Imperative
+function sum(a){
+    return function(b){
+        return a+b
+    }
+}
+
+sum(5)(10) //returns 15
+
+// Declarative
+let add = (a) => (b) =>  a + b
+
+add(5)(10) //returns 15
+
+// Pure function
+function pure(num){
+    return num * 2
+}
+
+// Impure function
+let multiplier = 4
+function impure(num){
+    return num * multiplier
+} 
+
+// Composition
+let arr = [10,20,30]
+
+arr.map(num => num * 2).reduce((acc,num) => acc + num ) //returns 120
+
+// Tasks
+
+let array = [1,2,3,4,5]
+
+// 1. For each number in the array console log "Number is:" followed by the number
+array.forEach(num => console.log("Number is:", num))
+
+// 2. Return a new array with cube of numbers from current array
+array.map(num => num*num*num)
+
+// 3. Return all the even numbers from the array
+array.filter(num => num % 2 === 0)
+
+// 4. Return only first odd number from the array
+array.find(num =>  num % 2 !== 0)
+
+// 5. Multiply first two numbers from the array and then multiply the result with next number
+array.reduce((acc,number) =>  num * acc, 1) //here 1 is intial value for accumulator
